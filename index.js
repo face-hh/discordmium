084bfba832fe52a6c541961298141c7add37ae81
Iliannnn <gamernaili.101010@gmail.com>
2023-01-03 17:45:44 +0100

    fix: bug fixes 💀

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ module.exports = async function browse(token, guildID, clearTime = 300000) {
 		const found = data.find((x) => x.id == msg.author.id);
 
 		if (found !== undefined) {
-			if(blacklist.includes(msg.content)) return msg.channel.createMessage('Blacklisted content detected, please refrain from using such keywords.');
+			if (blacklist.includes(msg.content)) return msg.channel.createMessage('Blacklisted content detected, please refrain from using such keywords.');
 
 			await page.keyboard.type(msg.content, { delay: 10 });
 
@@ -107,8 +107,8 @@ module.exports = async function browse(token, guildID, clearTime = 300000) {
 					await page.mouse.move(x, y);
 				}, clearTime);
 
-				if(blacklist.includes(int?.data?.options[0]?.value)) return int.createFollowup('Blacklisted content detected, please refrain from using such keywords.');
-				if (int.data.options) await page.goto(int.data.options[0].value);
+				if (blacklist.includes(int.data.options?.[0]?.value)) return int.createFollowup('Blacklisted content detected, please refrain from using such keywords.');
+				if (int.data.options && /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(int.data.options?.[0]?.value)) await page.goto(int.data.options[0].value);
 
 				const image = await page.screenshot();
 				const ids = [];
